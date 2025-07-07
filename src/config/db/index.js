60f074e26371948cb0c9dbf1baa9838a073d1193
Vanh260703 <131177903+Vanh260703@users.noreply.github.com>
2025-07-07 03:41:28 -0700

    fix url database

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb+srv://vietanh:<db_password>@todolist.e404wls.mongodb.net/?retryWrites=true&w=majority&appName=todolist');
+        await mongoose.connect('mongodb+srv://vietanh:Vietanh123@todolist.e404wls.mongodb.net/?retryWrites=true&w=majority&appName=todolist');
         console.log('Kết nối đến database thành công!!!');
     } catch (error) {
         console.log('Kết nối thất bại. Vui lòng thử lại!!!');
